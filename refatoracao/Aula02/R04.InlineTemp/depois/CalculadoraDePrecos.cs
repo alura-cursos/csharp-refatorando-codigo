8ee0d4c8a90082bd9dbb4940d22b051e7f2de536
marcelooliveira <mclricardo@gmail.com>
2017-11-07 14:06:35 -0200

    Aula 02
    Aula 02

---
+++ refatoracao/Aula02/R04.InlineTemp/depois/CalculadoraDePrecos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R04.InlineTemp.depois
+{
+    class CalculadoraDePrecos
+    {
+        bool TemDesconto(Pedido pedido)
+        {
+            return (pedido.ValorProdutos() > 1000);
+        }
+    }
+
+    class Pedido
+    {
+        private readonly DateTime clienteDesde;
+        private readonly decimal valorProdutos;
+
+        public Pedido(DateTime clienteDesde, decimal valorProdutos)
+        {
+            this.clienteDesde = clienteDesde;
+            this.valorProdutos = valorProdutos;
+        }
+
+        public decimal ValorProdutos()
+        {
+            return valorProdutos;
+        }
+
+        public bool TemDesconto()
+        {
+            //aqui NÃO É um bom exemlo para inline method!
+            bool clienteHaMaisDe5Anos = (DateTime.Today.Subtract(clienteDesde).TotalDays / 365) >= 5;
+            bool compraEspecial = valorProdutos > 1000;
+            return clienteHaMaisDe5Anos && compraEspecial;
+        }
+    }
+}
